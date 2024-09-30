8fe79c2783bcde10595267226cbb4d7ec2c6b865
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-09-30 16:18:19 +0200

    fix: Fix fallback behavior; apply stylua

--- lua/jsonfly/cache.lua
+++ lua/jsonfly/cache.lua
@@ -1,47 +1,42 @@
-local M = {};
+local M = {}
 
-local _cache = {};
+local _cache = {}
 
 ---@param buffer integer
 function M:cache_buffer(buffer, value)
-    _cache[buffer] = value;
+	_cache[buffer] = value
 end
 
 ---@param buffer integer
 function M:invalidate_buffer(buffer)
-    _cache[buffer] = nil;
+	_cache[buffer] = nil
 end
 
 ---@param buffer integer
 ---@return string[]|nil
 function M:get_cache(buffer)
-    return _cache[buffer];
+	return _cache[buffer]
 end
 
-local _listening_buffers = {};
+local _listening_buffers = {}
 
 ---@param buffer integer
 function M:register_listeners(buffer)
-    if _listening_buffers[buffer] then
-        return;
-    end
+	if _listening_buffers[buffer] then
+		return
+	end
 
-    _listening_buffers[buffer] = true;
+	_listening_buffers[buffer] = true
 
-    vim.api.nvim_buf_attach(
-        buffer,
-        false,
-        {
-            on_lines = function()
-                self:invalidate_buffer(buffer)
-            end,
-            on_detach = function()
-                self:invalidate_buffer(buffer)
-                _listening_buffers[buffer] = nil;
-            end,
-        }
-    );
+	vim.api.nvim_buf_attach(buffer, false, {
+		on_lines = function()
+			self:invalidate_buffer(buffer)
+		end,
+		on_detach = function()
+			self:invalidate_buffer(buffer)
+			_listening_buffers[buffer] = nil
+		end,
+	})
 end
 
-return M;
-
+return M
--- lua/jsonfly/parsers.lua
+++ lua/jsonfly/parsers.lua
@@ -15,120 +15,119 @@
 ---@field key_start number
 
 local PRIMITIVE_TYPES = {
-    string = true,
-    number = true,
-    boolean = true,
+	string = true,
+	number = true,
+	boolean = true,
 }
 local CONTAINS_CHILDREN_TYPES = {
-    [2] = true, -- Module / Javascript Object
-    [8] = true, -- Field
-    [18] = true, -- Array
-    [19] = true, -- Object
+	[2] = true, -- Module / Javascript Object
+	[8] = true, -- Field
+	[18] = true, -- Array
+	[19] = true, -- Object
 }
 
 local M = {}
 
 ---@param entry JSONEntry
 local function get_contents_from_json_value(entry)
-    local value = entry.value
+	local value = entry.value
 
-    if type(value) == "table" then
-        -- Recursively get the contents of the table
-        local contents = {}
+	if type(value) == "table" then
+		-- Recursively get the contents of the table
+		local contents = {}
 
-        for k, v in pairs(value) do
-            contents[k] = get_contents_from_json_value(v)
-        end
+		for k, v in pairs(value) do
+			contents[k] = get_contents_from_json_value(v)
+		end
 
-        return contents
-    else
-        return entry.value
-    end
+		return contents
+	else
+		return entry.value
+	end
 end
 
 ---@param t table|nil|string|number|boolean
 ---@return Entry[]
 function M:get_entries_from_lua_json(t)
-    if PRIMITIVE_TYPES[type(t)] or t == nil then
-        return {}
-    end
+	if PRIMITIVE_TYPES[type(t)] or t == nil then
+		return {}
+	end
 
-    local keys = {}
+	local keys = {}
 
-    for k, _raw_value in pairs(t) do
-        ---@type JSONEntry
-        local raw_value = _raw_value
-        ---@type Entry
-        local entry = {
-            key = tostring(k),
-            value = get_contents_from_json_value(raw_value),
-            position = {
-                line_number = raw_value.line_number,
-                key_start = raw_value.key_start,
-                value_start = raw_value.value_start,
-            }
-        }
-        table.insert(keys, entry)
+	for k, _raw_value in pairs(t) do
+		---@type JSONEntry
+		local raw_value = _raw_value
+		---@type Entry
+		local entry = {
+			key = tostring(k),
+			value = get_contents_from_json_value(raw_value),
+			position = {
+				line_number = raw_value.line_number,
+				key_start = raw_value.key_start,
+				value_start = raw_value.value_start,
+			},
+		}
+		table.insert(keys, entry)
 
-        local v = raw_value.value
+		local v = raw_value.value
 
-        if type(v) == "table" then
-            local sub_keys = M:get_entries_from_lua_json(v)
+		if type(v) == "table" then
+			local sub_keys = M:get_entries_from_lua_json(v)
 
-            for index=1, #sub_keys do
-                local sub_key = sub_keys[index]
+			for index = 1, #sub_keys do
+				local sub_key = sub_keys[index]
 
-                ---@type Entry
-                local entry = {
-                    key = k .. "." .. sub_key.key,
-                    value = sub_key.value,
-                    position = sub_key.position,
-                }
+				---@type Entry
+				local entry = {
+					key = k .. "." .. sub_key.key,
+					value = sub_key.value,
+					position = sub_key.position,
+				}
 
-                keys[#keys + 1] = entry
-            end
-        end
-    end
+				keys[#keys + 1] = entry
+			end
+		end
+	end
 
-    return keys
+	return keys
 end
 
 ---@param result Symbol
 ---@return string|number|table|boolean|nil
 function M:parse_lsp_value(result)
-    -- Object
-    if CONTAINS_CHILDREN_TYPES[result.kind] then
-        local value = {}
+	-- Object
+	if CONTAINS_CHILDREN_TYPES[result.kind] then
+		local value = {}
 
-        for _, child in ipairs(result.children) do
-            value[child.name] = M:parse_lsp_value(child)
-        end
+		for _, child in ipairs(result.children) do
+			value[child.name] = M:parse_lsp_value(child)
+		end
 
-        return value
-    -- Integer
-    elseif result.kind == 16 then
-        return tonumber(result.detail)
-    -- String
-    elseif result.kind == 15 then
-        return result.detail
-    -- Array
-    elseif result.kind == 18 then
-        local value = {}
+		return value
+	-- Integer
+	elseif result.kind == 16 then
+		return tonumber(result.detail)
+	-- String
+	elseif result.kind == 15 then
+		return result.detail
+	-- Array
+	elseif result.kind == 18 then
+		local value = {}
 
-        for i, child in ipairs(result.children) do
-            value[i] = M:parse_lsp_value(child)
-        end
+		for i, child in ipairs(result.children) do
+			value[i] = M:parse_lsp_value(child)
+		end
 
-        return value
-    -- null
-    elseif result.kind == 13 then
-        return nil
-    -- boolean
-    elseif result.kind == 17 then
-        return result.detail == "true"
-    end
+		return value
+	-- null
+	elseif result.kind == 13 then
+		return nil
+	-- boolean
+	elseif result.kind == 17 then
+		return result.detail == "true"
+	end
 end
-
 
 ---@class Symbol
 ---@field name string
@@ -149,45 +148,45 @@ end
 ---@param symbols Symbol[]
 ---@return Entry[]
 function M:get_entries_from_lsp_symbols(symbols)
-    local keys = {}
+	local keys = {}
 
-    for index=1, #symbols do
-        local symbol = symbols[index]
-        local key = symbol.name
+	for index = 1, #symbols do
+		local symbol = symbols[index]
+		local key = symbol.name
 
-        ---@type Entry
-        local entry = {
-            key = tostring(key),
-            value = M:parse_lsp_value(symbol),
-            position = {
-                line_number = symbol.range.start.line + 1,
-                key_start = symbol.range.start.character + 2,
-                -- The LSP doesn't return the start of the value, so we'll just assume it's 3 characters after the key
-                -- We assume a default JSON file like:
-                -- `"my_key": "my_value"`
-                -- Since we get the end of the key, we can just add 4 to get the start of the value
-                value_start = symbol.selectionRange["end"].character + 3,
-            }
-        }
-        keys[#keys + 1] = entry
+		---@type Entry
+		local entry = {
+			key = tostring(key),
+			value = M:parse_lsp_value(symbol),
+			position = {
+				line_number = symbol.range.start.line + 1,
+				key_start = symbol.range.start.character + 2,
+				-- The LSP doesn't return the start of the value, so we'll just assume it's 3 characters after the key
+				-- We assume a default JSON file like:
+				-- `"my_key": "my_value"`
+				-- Since we get the end of the key, we can just add 4 to get the start of the value
+				value_start = symbol.selectionRange["end"].character + 3,
+			},
+		}
+		keys[#keys + 1] = entry
 
-        if CONTAINS_CHILDREN_TYPES[symbol.kind] then
-            local sub_keys = M:get_entries_from_lsp_symbols(symbol.children)
+		if CONTAINS_CHILDREN_TYPES[symbol.kind] then
+			local sub_keys = M:get_entries_from_lsp_symbols(symbol.children)
 
-            for jindex=1, #sub_keys do
-                ---@type Entry
-                local entry = {
-                    key = key .. "." .. sub_keys[jindex].key,
-                    value = sub_keys[jindex].value,
-                    position = sub_keys[jindex].position,
-                }
+			for jindex = 1, #sub_keys do
+				---@type Entry
+				local entry = {
+					key = key .. "." .. sub_keys[jindex].key,
+					value = sub_keys[jindex].value,
+					position = sub_keys[jindex].position,
+				}
 
-                keys[#keys + 1] = entry
-            end
-        end
-    end
+				keys[#keys + 1] = entry
+			end
+		end
+	end
 
-    return keys
+	return keys
 end
 
 return M
